3d094ab46143a2bfc4725c1ac3e001b70bcbb832
alex <alexanderjamesramirez@gmail.com>
2022-10-22 16:48:15 -0700

    Updated AddFilm and added Film List Item class
    Basic setup for Add Film Activity and created RV Adapter class for each Film that will be displayed in Main Activity

--- app/src/main/java/info/alexanderramirez/filmcounter/AddFilm.java
+++ app/src/main/java/info/alexanderramirez/filmcounter/AddFilm.java
@@ -3,12 +3,32 @@ package info.alexanderramirez.filmcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddFilm extends AppCompatActivity {
+    private EditText filmTitleTextInput, filmWatchCountTextInput;
+    private Button addFilmBtn;
+    private DBHandler dbHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_film);
+
+        filmTitleTextInput = findViewById(R.id.FilmTitleTextInput);
+        filmWatchCountTextInput = findViewById(R.id.FilmWatchCountTextInput);
+        addFilmBtn = findViewById(R.id.AddFilmBtn);
+
+        dbHandler = new DBHandler(AddFilm.this);
+
+        addFilmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(AddFilm.this, "Film has been added!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/info/alexanderramirez/filmcounter/FilmRVAdapter.java
@@ -0,0 +1,14 @@
+package info.alexanderramirez.filmcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FilmRVAdapter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_film_rvadapter);
+    }
+}
